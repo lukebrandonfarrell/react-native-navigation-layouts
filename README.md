2f24a044f069730e62239446aeb61c4853af18b3
Luke Brandon Farrell <lukebrandonfarrell@gmail.com>
2019-06-12 14:56:36 +0200

    Added documentation and ability to generate animations

--- README.md
+++ README.md
@@ -1,6 +1,42 @@
 # react-native-navigation-layouts
 
-NOTICE: This library is not ready for production. Use at your own discretion.
+Utility library for generating layouts for [react-native-navigation](https://github.com/wix/react-native-navigation). This library exposes helper methods to generate the complex objects needed to define layouts in RNN.
+
+**Before:**
+
+```js
+Navigation.push(this.props.componentId, {
+  component: {
+    name: "CustomScreen",
+    passProps: {
+       mySpecialValue: false,
+    }
+  },
+});
+```
+
+**After:**
+
+```js
+import { component } from "react-native-navigation-layouts";
+
+Navigation.push(this.props.componentId, component("CustomScreen", { 
+  mySpecialValue: false 
+}));
+```
+
+## Documentation
+
+#### Animation
+
+The library also handles generating animation for your layouts. This can be done by using the `animate` method.
+
+```js
+ animations: {
+    ...animate("push", "x", 1000, 0, 200, 0, 'decelerate", true, "android"),
+ }
+```
+
 
 ## Authors
 
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { Platform } from "react-native";
+
 /** -------------------------------------------- */
 /**              Layout Generators               */
 /** -------------------------------------------- */
@@ -106,28 +108,48 @@ export const rightButtons = buttons => {
   return { rightButtons: [...buttons] };
 };
 
+
 /**
- * Generates animation properties for layout
+ * Generates a animation layout for RNN
  *
+ * @param command
+ * @param property
  * @param from
  * @param to
  * @param duration
  * @param delay
  * @param interpolation
+ * @param waitForRender
+ * @param platform
+ *
  * @return {{from: *, to: *, duration: *, startDelay: *, interpolation: string}}
  */
-export const animation = (
-  from,
-  to,
-  duration,
-  delay,
-  interpolation = "accelerate"
-) => {
-  return {
-    from,
-    to,
-    duration,
-    startDelay: delay,
-    interpolation
+export function animate(command, property, from, to, duration, delay, interpolation = "decelerate", waitForRender = true, platform){
+  const commands = ["setRoot", "push", "pop", "showModal", "dismissModal"];
+  const properties = ["x", "y", "alpha", "scaleX", "scaleY", "rotationX", "rotationY", "rotation"];
+
+  if(!commands.includes(command)) throw new Error(`First parameter 'command' must be one of: ${commands.join(", ")}`);
+  if(!properties.includes(property)) throw new Error(`Second parameter 'property' must be one of: ${properties.join(", ")}`);
+
+  const animation = {
+    [property]: {
+      from,
+      to,
+      duration,
+      startDelay: delay,
+      interpolation
+    }
   };
-};
+  const content = ["setRoot", "showModal", "dismissModal"].includes(command) ? { ...animation } : { content: { ...animation } }
+
+  if(!platform || platform === Platform.OS) {
+    return {
+      [command]: {
+        waitForRender,
+        ...content
+      }
+    }
+  }
+
+  return {};
+}
